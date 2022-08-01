6b32dd064426d83f3a7660a4102262850bb5fd0c
YunussEmree <86290901+YunussEmree@users.noreply.github.com>
2022-08-02 01:30:57 +0300

    Add files via upload

--- XRAY-CHECK/src/main/java/civciv/xraycheck/Events.java
+++ XRAY-CHECK/src/main/java/civciv/xraycheck/Events.java
@@ -1,10 +1,12 @@
 package civciv.xraycheck;
 
+import net.md_5.bungee.api.chat.HoverEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerChatEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,11 +24,19 @@ public class Events implements Listener {
 
         List<String> blockad = plugin.getConfig().getStringList(("xraycheck.blocks"));
 
+
         for(int i = 0; i<blockad.size(); i++) {
             if (event.getBlock().getType().toString(). equalsIgnoreCase(String.valueOf(blockad.get(i)))) {
                 String playername = event.getPlayer().getName();
                 String blockadi = String.valueOf(event.getBlock().getType());
-                Bukkit.broadcast("§c" + playername + "§e, break a§c " + blockadi + "§e.", "xray.check");
+                String perm = plugin.getConfig().getString("xraycheck.permission");
+                String text = plugin.getConfig().getString("xraycheck.warntext");
+                assert text != null;
+                String textyeni = text.replace("&", "§");
+                String textyeniyeni = textyeni.replace("%player%", playername);
+                String textyeniyeniyeni = textyeniyeni.replace("%block%", blockadi);
+                assert perm != null;
+                Bukkit.broadcast(textyeniyeniyeni, perm);
             }
         }
     }
